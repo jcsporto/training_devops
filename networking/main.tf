9c2a935de227e034cea1ebe279c434b116a644d0
Jean Porto <portojean@gmail.com>
2026-03-01 16:33:52 -0300

    feat(networking): configura state locking no backend S3
    - Adiciona dynamodb_table ao backend configuration
    - Corrige indentação e formatação do código
    - Comenta VPCs de exemplo para testes

--- networking/main.tf
+++ networking/main.tf
@@ -6,23 +6,29 @@ terraform {
     }
   }
 
-    backend "s3" {
-    bucket  = "terraform-state-files-training-devops"
-    key     = "networking/terraform.tfstate"
-    region  = "us-east-1"
-    profile = "training_devops"
+  backend "s3" {
+    bucket         = "terraform-state-files-training-devops"
+    key            = "networking/terraform.tfstate"
+    region         = "us-east-1"
+    profile        = "training_devops"
+    dynamodb_table = "terraform-state-locking"
   }
 }
 
 provider "aws" {
-  region = var.region
+  region  = var.region
   profile = "training_devops"
+
   assume_role {
     role_arn    = var.assume_role.role_arn
     external_id = var.assume_role.external_id
   }
 }
 
-resource "aws_vpc" "example" {
-  cidr_block = "10.0.0.0/16"
-}
+# resource "aws_vpc" "example" {
+#   cidr_block = "10.0.0.0/16"
+# }
+
+# resource "aws_vpc" "example2" {
+#   cidr_block = "10.0.0.0/16"
+# }

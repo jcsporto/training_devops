78bb06f0e06ac0c059740f3f3e2cb3ea69ef930b
Jean Porto <portojean@gmail.com>
2026-03-04 23:54:54 -0300

    feat: add variables configuration for Server module

---
+++ Server/variables.tf
@@ -0,0 +1,23 @@
+variable "region" {
+  default = "us-east-1"
+}
+
+variable "assume_role" {
+  type = object({
+    role_arn    = string
+    external_id = string
+  })
+  description = "AWS IAM role to assume with external ID"
+  # Define valores no arquivo terraform.tfvars (não versionado)
+}
+
+
+variable "ec2_resources" {
+  type = object({
+    key_pair_name = string
+  })
+
+  default = {
+    key_pair_name = "training-devops-key-pair"
+  }
+}

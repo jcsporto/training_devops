7f76f66344254fee0cb5f46f2b1eef05d5d2f47c
Jean Porto <portojean@gmail.com>
2026-03-14 15:10:22 -0300

    feat: add ec2 module variables for launch template, auto scaling group and shared inputs

---
+++ Server/modules/ec2/variables.tf
@@ -0,0 +1,54 @@
+variable "tags" {
+  type        = map(string)
+  description = "Tags to apply to resources"
+}
+
+variable "vpc_zone_identifier" {
+  type =  list(string)
+}
+
+variable "vpc_security_group_ids" {
+  type =  list(string)
+}
+
+variable "instance_profile_name" {
+  type = string
+}
+
+variable "key_name" {
+  type =  string
+}
+
+variable "image_id" {
+  type =  string
+}
+
+variable "launch_template" {
+  type = object({
+    name                                 = string
+    disable_api_stop                     = bool
+    disable_api_termination              = bool
+    instance_type                        = string
+    instance_initiated_shutdown_behavior = string
+    ebs = object({
+      size                  = number
+      delete_on_termination = bool
+    })
+  })
+}
+
+variable "auto_scaling_group" {
+  type = object({
+    name                      = string
+    min_size                  = number
+    max_size                  = number
+    desired_capacity          = number
+    health_check_grace_period = number
+    health_check_type         = string
+    instance_maintenance_policy = object({
+      min_healthy_percent = number
+      max_healthy_percent = number
+    })
+  }) 
+}
+
